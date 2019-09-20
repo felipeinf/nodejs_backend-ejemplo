43fe64b02b36205485c1cc80f605aa073ecb37a0
Felipe mac <fconcha.almeida@gmail.com>
2019-09-19 22:24:50 -0300

    refactor: se agrego el middleware body-parser

--- servidor.js
+++ servidor.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
 const puerto = 3001;
+
+app.use(bodyParser.json())
 app.listen(puerto);
 
 console.log(`Servidor escuchando en http://localhost:${puerto}`);
