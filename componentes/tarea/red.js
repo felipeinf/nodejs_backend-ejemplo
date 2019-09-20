fa189a3920c724ce15ccc6e0f5ef45510340b6f8
Felipe mac <fconcha.almeida@gmail.com>
2019-09-20 00:23:43 -0300

    fix: se requirio el controlador en la capa de red

--- componentes/tarea/red.js
+++ componentes/tarea/red.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const controlador = require('./controlador');
 
 router.post('/:uid', async (req, res) => {
     const body = req.body;
